87a343dc02a1095f14b51275e7baf7fd67106443
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-06-24 10:25:04 +0900

    added resolve for weatherData todo: refactor

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,13 +2,35 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl',
+            resolve: {
+                weatherData: ['$q', 'weatherSvc', function ($q, weatherSvc, options) {
+                    var lat = 0;
+                    var lon = 0;
+                    var deferred = $q.defer();
 
-.controller('View1Ctrl', [function() {
+                    navigator.geolocation.getCurrentPosition(
+                        deferred.resolve,
+                        deferred.reject,
+                        options);
 
-}]);
+                    deferred.promise.then(function (position) {
+                        var data = weatherSvc.query(position.coords.latitude, position.coords.longitude);
+                    });
+                    return deferred.promise.then(function (position) {
+                            return weatherSvc.query(position.coords.latitude, position.coords.longitude);
+                        }
+                    );
+                }]
+            }
+        });
+    }])
+
+    .controller('View1Ctrl', ['$scope', 'weatherData', function ($scope, weatherData) {
+        $scope.weatherData = weatherData.data;
+        console.log('test');
+        console.log($scope.weatherData.city.name);
+    }]);
