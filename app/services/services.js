245a54213fe54f1e2ad5cfb2824ea844f44b3901
Michael Hobbs <michael.lee.hobbs@gmail.com>
2016-06-19 13:01:47 -0400

    Fixed api key

--- app/services/services.js
+++ app/services/services.js
@@ -1,6 +1,34 @@
+
+// Added this function as a quick fix for OpenWeatherMap's new requirement of an API key
+function loadJSON(callback) {
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', '/fccWeather/app/env/keys.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
+
 angular.module('myApp.services', [])
     .factory('weatherSvc', ['$http', function ($http) {
         'use strict';
+        var apiKey;
+
+        // Added this function as a quick fix for OpenWeatherMap's new requirement of an API key
+        (function init(){
+            loadJSON(function(response){
+                apiKey = JSON.parse(response).openWeatherMap;
+                console.log(apiKey)
+            });
+        })();
+
+
         function rangeLimit(value, min, max) {
             value = (value >= min) ? value : min;
             value = (value <= max) ? value : max;
@@ -9,6 +37,7 @@ angular.module('myApp.services', [])
 
         var sdo = {
             query: function (lat, lon, refresh) {
+                console.log(apiKey)
                 // validations
                 refresh = refresh || false;
                 // valid lat = -90...90
@@ -18,7 +47,7 @@ angular.module('myApp.services', [])
 
                 var promise = $http({
                     method: 'GET',
-                    url: 'http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + lat + '&lon=' + lon + '&mode=json'
+                    url: 'http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + lat + '&lon=' + lon + '&mode=json' + '&APPID=' + apiKey
                 });
                 promise.success(function (data, status, headers, conf) {
                     return data;
